553aa44e7954a55174b19ba7307320c119abaaf1
Samuel Hope <samuelhope805@gmail.com>
2026-03-07 14:08:53 +0300

    staff model fix

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -3,4 +3,4 @@
 export { default as User } from './User';
 export { default as Invitation } from './Invitation';
 export { default as SecurityLog } from './SecurityLog';
-export { default as Staff } from './Staff';
+//export { default as Staff } from './Staff';
