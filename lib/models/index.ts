5f7d61175902fc48d5dea4306a061f34a1f8afa1
Samuel Hope <samuelhope805@gmail.com>
2026-06-23 13:24:58 +0300

    remove userts

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -1,6 +1,6 @@
-// lib/models/index.ts
+// // lib/models/index.ts
 
-export { default as User } from './User';
-export { default as Invitation } from './Invitation';
-export { default as SecurityLog } from './SecurityLog';
-//export { default as Staff } from './Staff';
+// export { default as User } from './User';
+// export { default as Invitation } from './Invitation';
+// export { default as SecurityLog } from './SecurityLog';
+// //export { default as Staff } from './Staff';
